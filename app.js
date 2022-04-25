c02788d0cc0c08536c1b58c8dcdd09671bad20c8
Luke McCallum <luke.mccallum122@gmail.com>
2022-04-26 00:30:49 +1200

    complete design overhaul

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const switcher = document.querySelector('#mode');
 
-switcher.addEventListener('click', function() {
+switcher.addEventListener('click', function () {
     document.body.classList.toggle('light-mode')
 
     var className = document.body.className;
-    if(className == "dark-mode") {
-        this.textContent = "Light";
+    if (className == "dark-mode") {
+        this.textContent = "Light Mode";
     } else {
-        this.textContent = "Dark";
+        this.textContent = "Dark Mode";
     }
 
     console.log("Current Theme: " + this.textContent)
@@ -15,12 +15,21 @@ switcher.addEventListener('click', function() {
 
 var search = document.getElementById("searchTerm");
 
-search.addEventListener("keypress", function(keyPressed) {
+search.addEventListener("keypress", function (keyPressed) {
     if (keyPressed.key === 'Enter') {
         location.replace("https://www.google.com/search?q=" + search.value + "");
     }
 });
 
-search.addEventListener("click", function() {
+search.addEventListener("click", function () {
     search.value = "";
 });
+
+const body = document.querySelector('body');
+body.addEventListener('load', startTime());
+function startTime() {
+    const today = new Date();
+    document.getElementById("date").innerHTML = today.toDateString();
+    document.getElementById("time").innerHTML = today.toLocaleTimeString();
+    setTimeout(startTime, 1000);
+};
