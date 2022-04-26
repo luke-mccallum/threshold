d40316d4515e9263278d776625579af8269d6b38
Luke McCallum <luke.mccallum122@gmail.com>
2022-04-26 18:24:00 +1200

    changed how search placeholder text is displayed

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-const switcher = document.querySelector('#mode');
-
-switcher.addEventListener('click', function () {
+const MODE_SWITCH = document.querySelector('#mode');
+MODE_SWITCH.addEventListener('click', function () {
     document.body.classList.toggle('light-mode')
-
-    var className = document.body.className;
+    let className = document.body.className;
     if (className == "dark-mode") {
         this.textContent = "Light Mode";
     } else {
         this.textContent = "Dark Mode";
     }
-
     console.log("Current Theme: " + this.textContent)
 });
 
-var search = document.getElementById("searchTerm");
 
+let search = document.getElementById("searchTerm");
 search.addEventListener("keypress", function (keyPressed) {
     if (keyPressed.key === 'Enter') {
+        keyPressed.preventDefault();
         location.replace("https://www.google.com/search?q=" + search.value + "");
     }
 });
 
-search.addEventListener("click", function () {
-    search.value = "";
-});
 
-const body = document.querySelector('body');
-body.addEventListener('load', startTime());
+const BODY = document.querySelector('body');
+BODY.addEventListener('load', startTime());
 function startTime() {
-    const today = new Date();
-    document.getElementById("date").innerHTML = today.toDateString();
-    document.getElementById("time").innerHTML = today.toLocaleTimeString();
+    const DATE = new Date();
+    document.getElementById("date").innerHTML = DATE.toDateString();
+    document.getElementById("time").innerHTML = DATE.toLocaleTimeString();
     setTimeout(startTime, 1000);
 };
