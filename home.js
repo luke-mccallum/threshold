6b1510bd1c9b5003e3d8462785844bdcd5e76c3f
Luke McCallum <luke.mccallum122@gmail.com>
2022-08-20 00:36:04 +1200

    added functions for link removal/addition

--- home.js
+++ home.js
@@ -10,31 +10,32 @@ window.onload = function () {
     }
     // Loading center image
     if (response.centerImage != undefined || "custom") {
-        console.log("Successfully loaded " + response.centerImage);
-        document.getElementById("mainImage").src = response.centerImage;
-    }})
+      console.log("Successfully loaded " + response.centerImage);
+      document.getElementById("mainImage").src = response.centerImage;
+    }
+  });
 };
 
 // Live updating settings
 chrome.storage.onChanged.addListener(function (changes, namespace) {
-    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-      console.log(
-        `Storage key "${key}" in namespace "${namespace}" changed.`,
-        `Old value was "${oldValue}", new value is "${newValue}".`
-      );
-      if (key === "colorMode") {
-        document.body.className = newValue;
-      }
-      if (key === "centerImage") {
-        if (newValue === "custom") {
-            let customLink = prompt("Enter a link to the source of your image/gif");
-            chrome.storage.sync.set({centerImage: customLink});
-        } else {
-            document.getElementById("mainImage").src = newValue;
-        }
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+    if (key === "colorMode") {
+      document.body.className = newValue;
+    }
+    if (key === "centerImage") {
+      if (newValue === "custom") {
+        let customLink = prompt("Enter a link to the source of your image/gif");
+        chrome.storage.sync.set({ centerImage: customLink });
+      } else {
+        document.getElementById("mainImage").src = newValue;
       }
     }
-  });
+  }
+});
 
 // Google search functionality
 let search = document.getElementById("searchTerm");
@@ -53,4 +54,39 @@ function startTime() {
   document.getElementById("date").innerHTML = DATE.toDateString();
   document.getElementById("time").innerHTML = DATE.toLocaleTimeString();
   setTimeout(startTime, 1000);
+}
+
+// Adding a new category
+function newCategory(name) {
+  let category = document.createElement("figure");
+  category.setAttribute("id", name);
+
+  let figcaption = document.createElement("figcaption");
+  figcaption.innerHTML = name;
+  category.appendChild(figcaption);
+
+  let list = document.createElement("ul");
+  list.setAttribute("id", name + "List");
+  category.appendChild(list);
+  document.getElementById("leftbar").appendChild(category);
+}
+
+// Removing a category
+function removeCategory(name) {
+  document.getElementById(name).remove();
+}
+
+// Adding a new link
+function newLink(name, link, category) {
+  let listElement = document.createElement("li");
+  let linkElement = document.createElement("a");
+  linkElement.href = link;
+  linkElement.innerHTML = name;
+  listElement.appendChild(linkElement);
+  document.getElementById(category + "List").appendChild(listElement);
+}
+
+// Removing a link
+function removeLink(name) {
+  document.getElementById(name + "Link").remove();
 }
