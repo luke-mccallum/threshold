e0a6bcec3484c1493ac1a00118d9962cf139bf19
Luke McCallum <luke.mccallum122@gmail.com>
2022-08-19 22:44:56 +1200

    fixed both themes showing on first load

--- home.js
+++ home.js
@@ -1,28 +1,30 @@
-'use strict'
+"use strict";
 
 // Activating light or dark mode
-window.onload =  function () {
-    chrome.storage.sync.get(['colorMode'], function(response) {
-        console.log('Successfully loaded ' + response.colorMode)
-        document.body.className = (response.colorMode)
-    })
-}
+window.onload = function () {
+  chrome.storage.sync.get(["colorMode"], function (response) {
+    if (response.colorMode != undefined) {
+      console.log("Successfully loaded " + response.colorMode);
+      document.body.className = response.colorMode;
+    }
+  });
+};
 
 // Google search functionality
 let search = document.getElementById("searchTerm");
 search.addEventListener("keypress", function (keyPressed) {
-    if (keyPressed.key === 'Enter') {
-        keyPressed.preventDefault();
-        location.replace("https://www.google.com/search?q=" + search.value + "");
-    }
+  if (keyPressed.key === "Enter") {
+    keyPressed.preventDefault();
+    location.replace("https://www.google.com/search?q=" + search.value + "");
+  }
 });
 
 // Date and time
-const BODY = document.querySelector('body');
-BODY.addEventListener('load', startTime());
+const BODY = document.querySelector("body");
+BODY.addEventListener("load", startTime());
 function startTime() {
-    const DATE = new Date();
-    document.getElementById("date").innerHTML = DATE.toDateString();
-    document.getElementById("time").innerHTML = DATE.toLocaleTimeString();
-    setTimeout(startTime, 1000);
-};
+  const DATE = new Date();
+  document.getElementById("date").innerHTML = DATE.toDateString();
+  document.getElementById("time").innerHTML = DATE.toLocaleTimeString();
+  setTimeout(startTime, 1000);
+}
